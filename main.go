ea705c267b565b3fb914919bb07d58b67ace3f5d
Maho Shojo Kierkegaard <a201109@foxmail.com>
2022-08-05 06:20:20 +0800

    enable mongodb support

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/Mrs4s/go-cqhttp/modules/mime" // mime检查模块
 	_ "github.com/Mrs4s/go-cqhttp/modules/silk" // silk编码模块
 	// 其他模块
-	// _ "github.com/Mrs4s/go-cqhttp/db/mongodb"    // mongodb 数据库支持
+	 _ "github.com/Mrs4s/go-cqhttp/db/mongodb"    // mongodb 数据库支持
 	// _ "github.com/Mrs4s/go-cqhttp/modules/pprof" // pprof 性能分析
 )
 
